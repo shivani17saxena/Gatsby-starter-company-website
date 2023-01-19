e875b6c5ac6b36e90a961d80cca21460e0365b85
Shivani Saxena <shivani.saxena95@gmail.com>
2023-01-19 22:46:57 +0530

    Form submit netlify integration

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { navigate } from "gatsby-link";
 import * as styles from '../styles/form.module.css'
 
+
 const Form = () => {
+
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message:""
+  })
+
+  const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
+  const handleChange = e => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = e => {
+    const myForm = e.target;
+    //const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...formState })
+    })
+      .then(() => navigate("/thanks"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+
+  }
+
   return (
 
 
@@ -14,14 +52,44 @@ const Form = () => {
                 Scale fast with out on-demand developers
             </h3>
         </div>
-        <div>
-          <input name='name' placeholder='Your Name *' />
-          <input name='email' placeholder='Your Work Email *' />
-          <textarea className={styles. message} rows='3' name='message' placeholder='How can we help you?' />
-        </div>
-        <div>
-          <button name='btn'>Power Up Your Team</button>
-        </div>
+        <form 
+          onSubmit={handleSubmit} 
+          name="contact" 
+          action="/thanks"
+          method="post" 
+          data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+          <div>
+            <input 
+              id='name' 
+              type='text'
+              name='name' 
+              onChange={handleChange}
+              value={formState.name}
+              placeholder='Your Name *' 
+            />
+            <input 
+              type='text'
+              id='email'
+              onChange={handleChange}
+              value={formState.email}
+              name='email' 
+              placeholder='Your Work Email *' 
+            />
+            <textarea 
+              className={styles.message} 
+              rows='3' 
+              name='message' 
+              id='message'
+              onChange={handleChange}
+              value={formState.message}
+              placeholder='How can we help you?' 
+            />
+          </div>
+          <div>
+            <button name='btn' type='submit'>Power Up Your Team</button>
+          </div>
+        </form>
     </div>
   )
 }
